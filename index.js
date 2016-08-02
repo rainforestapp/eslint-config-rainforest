feebe1e9f7409993246087d336e99648beb603cc
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2016-08-02 15:13:20 +0100

    updated config

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "airbnb",
-    "import/no-unresolved"
-  ],
-  "env": {
-    "browser": true,
-    "jest": true,
-    "jasmine": true,
-    "commonjs": true
+  'parser': 'babel-eslint',
+  'extends': 'airbnb',
+  'env': {
+    'browser': true,
+    'jest': true,
+    'jasmine': true,
+    'commonjs': true
   },
-  "rules": {
-    "flow-vars/define-flow-type": 1,
-    "flow-vars/use-flow-type": 1,
-    "valid-jsdoc": 2,
-    "react/sort-comp": [1, {
+  'rules': {
+    'flow-vars/define-flow-type': 1,
+    'flow-vars/use-flow-type': 1,
+    'valid-jsdoc': 2,
+    'react/sort-comp': [1, {
       order: [
         'static-methods',
         'lifecycle',
@@ -24,30 +21,37 @@ module.exports = {
       ]
     }],
 
-    "one-var-declaration-per-line": [2, "initializations"],
+    'one-var-declaration-per-line': [2, 'initializations'],
 
-    "one-var": [2, {
-      "uninitialized": "always",
+    'one-var': [2, {
+      'uninitialized': 'always',
     }],
 
-    "react/jsx-uses-react": 2,
-    "react/jsx-uses-vars": 2,
-    "react/no-multi-comp": 0,
-    "react/prop-types": 0,
-    "react/react-in-jsx-scope": 2,
-    "react/wrap-multilines": 2,
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/no-multi-comp': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 2,
+    'react/wrap-multilines': 2,
 
-    "no-underscore-dangle": 0,
-    "comma-dangle": [2, "always-multiline"],
-    "id-length": [2, {"exceptions": ["_", "$"]}],
-    "new-cap": [2, {"capIsNewExceptions": ["Iterable", "Seq", "Collection", "Map", "OrderedMap", "List", "Stack", "Set", "OrderedSet", "Record", "Range", "Repeat"]}]
+    'no-underscore-dangle': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'id-length': [2, {'exceptions': ['_', '$']}],
+    'new-cap': [2, {'capIsNewExceptions': ['Iterable', 'Seq', 'Collection', 'Map', 'OrderedMap', 'List', 'Stack', 'Set', 'OrderedSet', 'Record', 'Range', 'Repeat']}],
+
+    // we're disabling the default no-duplicate-imports rule and enabling
+    // the import plugin equivalent which differentiates between normal imports
+    // and flow type imports
+    'no-duplicate-imports': 0,
+    'import/no-duplicates': 1
   },
-  "plugins": [
-    "flow-vars",
-    "react",
-    "babel"
+  'plugins': [
+    'flow-vars',
+    'react',
+    'babel',
+    'import'
   ],
-  "globals": {
-    "analytics": true
+  'globals': {
+    'analytics': true
   }
 }
